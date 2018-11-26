d97d0e5f41733b155fbe4ee7a0a0b36094ab2d51
sanjaypj <sanjaypjayan2000@gmail.com>
2018-11-26 22:04:15 +0530

    comment fixed

--- blog/models.py
+++ blog/models.py
@@ -13,12 +13,22 @@ class Tag(models.Model):
 	def __str__(self):
 		return self.name
 
+class Comment(models.Model):
+	name = models.CharField(max_length=30)
+	body = models.CharField(max_length=100)
+	date = models.DateTimeField()
+
+	def __str__(self):
+		return self.name
+
 class Blog(models.Model):
     title = models.CharField(max_length=30)
     body = models.CharField(max_length=800)
     date = models.DateTimeField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
+    comments = models.ManyToManyField(Comment)
+        
 
 
     def __str__(self):
@@ -26,12 +36,3 @@ class Blog(models.Model):
 
     class Meta:
         ordering = ('title',)
-
-class Comment(models.Model):
-	name = models.CharField(max_length=30)
-	body = models.CharField(max_length=100)
-	date = models.DateTimeField()
-	blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-
-	def __str__(self):
-		return self.name
